c9e4e307b11c65ca582b096c1a51282889a4a62a
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-01-15 08:12:19 -0300

    Cosmetic fixes

--- javascript/index.js
+++ javascript/index.js
@@ -6,17 +6,16 @@ function max(a, b) {
   return a > b ? a : b;
 }
   
-function knapSack(W, wt, val) {
-  const n = wt.length
+function knapSack(W, weights, values) {
+  const n = weights.length
   const K = new Array(n + 1);
-  let i, w;
 
-  for (i = 0; i <= n; i++) {
+  for (let i = 0; i <= n; i++) {
     K[i] = new Array(W + 1);
-    for (w = 0; w <= W; w++) {
+    for (let w = 0; w <= W; w++) {
       if (i == 0 || w == 0) K[i][w] = 0;
-      else if (wt[i - 1] <= w)
-        K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+      else if (weights[i - 1] <= w)
+        K[i][w] = max(values[i - 1] + K[i - 1][w - weights[i - 1]], K[i - 1][w]);
       else K[i][w] = K[i - 1][w];
     }
   }
@@ -29,11 +28,11 @@ const transactions = JSON.parse(fs.readFileSync('data/transactions.json'))
 const df = transactions.map(o => ({ ...o, latency: latencies[o.bank_country_code]}))
 
 const limit = 1000
-const wt = Object.values(df.map(o => o.latency))
-const val = Object.values(df.map(o => o.amount))
+const weights = df.map(o => o.latency)
+const values = df.map(o => o.amount)
 
 const startTime = performance.now()
-knapSack(limit, wt, val)
+knapSack(limit, weights, values)
 const endTime = performance.now()
 const timeDiff = endTime - startTime
 
